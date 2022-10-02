17fe89ca2ec853a504bd1bec92fa0d858b2eec06
Aadiba Haque <41478933+adibsxion19@users.noreply.github.com>
2022-10-01 22:24:10 -0400

    Add files via upload

---
+++ db/db.py
@@ -0,0 +1,12 @@
+"""
+This file will manage interactions with our data store.
+At first, it will just contain stubs that return fake data.
+Gradually, we will fill in actual calls to our datastore.
+"""
+
+
+def fetch_pets():
+    """
+    A function to return all pets in the data store.
+    """
+    return {"tigers": 2, "lions": 3, "zebras": 1}
