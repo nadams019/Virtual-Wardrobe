3cc969e66b606d9a42a1187062f8d74ec0012051
Bismah <bismahshahid786@gmail.com>
2022-11-06 22:53:42 -0500

    python file for the different dropdown details

---
+++ clothing_item_type.py
@@ -0,0 +1,44 @@
+# This module encapsulates the details about the different dropdowns
+
+ITEM_TYPE = 'itemTypeButton'
+SEASON = 'SeasonButton'
+OCCASION = 'occasionButton'
+AESTHETIC = 'aestheticButton'
+
+char_types = {ITEM_TYPE: {'Tops', 'Bottoms', 'Dresses', 'Accessories', 'Jackets'},
+              SEASON: {'Fall', 'Winter', 'Summer', 'Spring'},
+              OCCASION: {'Casual', 'Formal', 'Semi-formal', 'Business', 'Party'},
+              AESTHETIC: {'Grunge', 'Academia', 'Streetwear', 'Cottagecore', 'Indie', 'Soft girl'},
+              }
+
+
+def add_char_type(type_name, selections):
+    if char_type_exists(type_name):
+        raise ValueError(f'Char type exists: {type_name=}')
+    char_types[type_name] = selections
+
+
+def del_char_type(type_name):
+    if char_type_exists(type_name):
+        del char_types[type_name]
+
+
+def char_type_exists(type_name):
+    return type_name in char_types
+
+
+def get_char_types():
+    return list(char_types.keys())
+
+
+def get_char_type_details(char_type):
+    return char_types.get(char_type, None)
+
+
+def main():
+    char_types = get_char_types()
+    print(char_types)
+
+
+if __name__ == '__main__':
+    main()
