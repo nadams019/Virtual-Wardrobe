e0877bf168e43544541c37750dc6c7787a0fe8bf
bethanysaunders <bsaun99@gmail.com>
2022-11-13 21:06:07 -0500

    adding closet browsing testing

--- db/closet_browse.py
+++ db/closet_browse.py
@@ -1,6 +1,7 @@
 """
 This module encapsulates details about our closet browsing page.
 """
+import db.db_connect as dbc
 
 TEST_CATEGORY_NAME = 'Test Category'
 CATEGORY = 'category'
@@ -9,42 +10,64 @@ SEASON = 'season'
 OCCASION = 'occasion'
 AESTHETIC = 'aesthetic'
 RANDOM = 'random'
-BROWSE = 'browse'
 
-# We expect the user database to change frequently:
-# For now, we will consider EMAIL to be
-# our mandatory fields.
-
-REQUIRED_FLDS = [CATEGORY]
-closet = {TEST_CATEGORY_NAME: {SEASON: 'winter', OCCASION: 'Prom',
-                               AESTHETIC: 'Classic', RANDOM: 'No'},
-          'handle': {SEASON: 'summer', OCCASION: 'School',
-                     AESTHETIC: 'Emo', RANDOM: 'Yes'}}
+REQUIRED_FLDS = [CATEGORY, SEASON, OCCASION, AESTHETIC, RANDOM]
+closet = {TEST_CATEGORY_NAME: {SEASON: 'winter', OCCASION: 'formal',
+                               AESTHETIC: 'vintage', RANDOM: 'No'},
+          'handle': {SEASON: 'summer', OCCASION: 'casual',
+                     AESTHETIC: 'preppy', RANDOM: 'Yes'}}
 
 
-def category_select(category):
+CLOTHING_KEY = 'item'
+CLOTHING_COLLECT = 'closet'
+
+"""
+Possible Category Types
+Season: Winter, Spring, Summer, Fall
+Occasion: Formal, Casual, Going-Out, Holiday
+Aesthetic: Vintage, Casual, Gothic, Streetwear, Preppy
+Random: Yes or No
+"""
+
+
+def clothing_exists(item):
     """
-    :param category
-    :return:
+    Returns whether or not a clothing item exists.
     """
-    return
+    return item in closet
 
 
-def clothing_exists(clothing):
-    return clothing in closet
+def get_clothing_dict():
+    dbc.connect_db()
+    return dbc.fetch_all_as_dict(CLOTHING_KEY, CLOTHING_COLLECT)
 
 
 def get_clothes():
-    return list(closet.keys())
+    dbc.connect_db()
+    return dbc.fetch_all(CLOTHING_COLLECT)
 
 
 def get_clothing_details(item):
     return closet.get(item, None)
 
 
+def add_clothing(item, details):
+    if not isinstance(item, str):
+        raise TypeError(f'Wrong type for clothing item: {type(item)=}')
+    if not isinstance(details, dict):
+        raise TypeError(f'Wrong type for clothing details: {type(details)=}')
+    for field in REQUIRED_FLDS:
+        if field not in details:
+            raise ValueError(f'Required {field=} missing from clothing details.')
+    closet[item] = details
+
 def main():
-    clothes = get_clothes()
-    print(f'{clothes=}')
+    print('Retrieving closet as a list:')
+    closet = get_clothes()
+    print(f'{closet=}')
+    print('Getting closet as a dict:')
+    closet = get_clothing_dict()
+    print(f'{closet=}')
     print(f'{get_clothing_details(TEST_CATEGORY_NAME)=}')
 
 
--- db/tests/test_closet-browse.py
+++ db/tests/test_closet-browse.py
@@ -4,11 +4,34 @@ import db.closet_browse as browse
 
 
 def test_get_clothes():
-    result = browse.get_clothes()
-    assert isinstance(result, list)
-    assert len(result) > 1
+    browsing = browse.get_clothes()
+    assert isinstance(browsing, list)
+    assert len(browsing) > 1
 
 
 def test_get_clothing_details():
-    cloth_deets = browse.get_clothing_details(browse.TEST_CATEGORY_NAME)
-    assert isinstance(cloth_deets, dict)
+    browse_dets = browse.get_clothing_details(browse.TEST_GAME_NAME)
+    assert isinstance(browse_dets, dict)
+
+
+def test_add_wrong_name_type():
+    with pytest.raises(TypeError):
+        browse.add_clothing(7, {})
+
+
+def test_add_wrong_details_type():
+    with pytest.raises(TypeError):
+        browse.add_clothing('a new clothing item', [])
+
+
+def test_add_missing_field():
+    with pytest.raises(ValueError):
+        browse.add_clothing('a new clothing item', {'foo': 'bar'})
+
+
+def test_add_clothing():
+    details = {}
+    for field in browse.REQUIRED_FLDS:
+        details[field] = 2
+    browse.add_clothing(browse.TEST_GAME_NAME, details)
+    assert browse.clothing_exists(browse.TEST_GAME_NAME)
