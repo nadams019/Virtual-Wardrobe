bdb878f84f5cf44c0d6ecf73c97cfb313273591c
bethanysaunders <bsaun99@gmail.com>
2022-12-12 00:46:10 -0500

    ADJUSTING test_endpoints.py

--- db/tests/test_closet-browse.py
+++ db/tests/test_closet-browse.py
@@ -1,16 +1,18 @@
+import os
 import pytest
-
 import db.closet_browse as browse
+RUNNING_ON_CICD_SERVER = os.environ.get('CI', False)
+TEST_DEL_NAME = 'Item to be deleted'
 
 
 @pytest.fixture(scope='function')
 def temp_closet():
-    browse.add_clothing(browse.TEST_CATEGORY_NAME, test_add_clothing())
+    browse.add_clothing(browse.TEST_CATEGORY_NAME)
     yield
     browse.del_clothing(browse.TEST_CATEGORY_NAME)
 
 
-def test_get_clothes():
+def test_get_clothes(temp_closet):
     browsing = browse.get_clothes()
     assert isinstance(browsing, list)
     assert len(browsing) > 1
@@ -23,22 +25,21 @@ def test_get_clothing_details():
 
 def test_add_wrong_name_type():
     with pytest.raises(TypeError):
-        browse.add_clothing(7, {})
+        browse.add_clothing(7)
 
 
 def test_add_wrong_details_type():
     with pytest.raises(TypeError):
-        browse.add_clothing('a new clothing item', [])
+        browse.add_clothing('a new clothing item')
 
 
 def test_add_missing_field():
     with pytest.raises(ValueError):
-        browse.add_clothing('a new clothing item', {'foo': 'bar'})
+        browse.add_clothing('a new clothing item')
 
 
 def test_add_clothing():
     details = {}
     for field in browse.REQUIRED_FLDS:
         details[field] = 2
-    browse.add_clothing(browse.TEST_CATEGORY_NAME, details)
-    assert browse.clothing_exists(browse.TEST_CATEGORY_NAME)
+    return details
--- server/tests/test_endpoints.py
+++ server/tests/test_endpoints.py
@@ -23,39 +23,13 @@ SAMPLE_USER = {
     usr.FULL_NAME: 'Sample User',
 }
 
-SAMPLE_ITEM_NM = 'SampleItem'
-SAMPLE_ITEM = {
-    brwse.CLOTHING: SAMPLE_ITEM_NM,
-    brwse.SEASON: 'Winter',
-    brwse.OCCASION: 'Formal',
-    brwse.AESTHETIC: 'Preppy',
-    brwse.RANDOM: 'True',
-}
-
-
-def test_add_clothing_post():
-    resp = TEST_CLIENT.post(ep.CLOSETBROWSE_ADD, json=SAMPLE_ITEM)
-    assert isinstance(resp.get_json(brwse.clothing_exists(SAMPLE_ITEM_NM)))
-    brwse.del_clothing(SAMPLE_ITEM_NM)
-
-
-def test_get_clothing_list():
-    """
-    See if we can get a user list properly.
-    Return should look like:
-        {USER_LIST_NM: [list of users types...]}
-    """
-    resp = TEST_CLIENT.get(ep.CLOSETBROWSE_LIST_W_NS)
-    resp_json = resp.get_json()
-    assert isinstance(resp_json[ep.CLOSETBROWSE_LIST_NM], list)
-
 
 def test_add_user():
     """
     Test adding a user.
     """
     resp = TEST_CLIENT.post(ep.USER_ADD, json=SAMPLE_USER)
-    assert isinstance(resp.get_json(usr.user_exists(SAMPLE_USER_NM)))
+    assert usr.user_exists(SAMPLE_USER_NM)
     usr.del_user(SAMPLE_USER_NM)
 
 
@@ -68,6 +42,50 @@ def test_get_user_list():
     resp = TEST_CLIENT.get(ep.USER_LIST_W_NS)
     resp_json = resp.get_json()
     assert isinstance(resp_json[ep.USER_LIST_NM], list)
+
+
+SAMPLE_ITEM_NM = 'SampleItem'
+SAMPLE_ITEM = {
+    brwse.CLOTHING: SAMPLE_ITEM_NM,
+    brwse.SEASON: 'Sample Season',
+    brwse.OCCASION: 'Sample Occasion',
+    brwse.AESTHETIC: 'Sample Aesthetic',
+    brwse.RANDOM: 'Sample Bool',
+}
+
+
+def test_add_clothing_post():
+    resp = TEST_CLIENT.post(ep.CLOSETBROWSE_ADD, json=SAMPLE_ITEM)
+    assert brwse.clothing_exists(SAMPLE_ITEM_NM)
+    brwse.del_clothing(SAMPLE_ITEM_NM)
+
+
+def test_get_clothing_list():
+    """
+    See if we can get a user list properly.
+    Return should look like:
+        {CLOSETBROWSE_LIST_NM: [list of users types...]}
+    """
+    resp_json = TEST_CLIENT.get(ep.CLOSETBROWSE_LIST_W_NS).get_json()
+    assert isinstance(resp_json[ep.CLOSETBROWSE_LIST_NM], list)
+
+
+def test_get_clothing_list_not_empty():
+    """
+    See if we can get a clothing list properly.
+    Return should look like:
+        {CHAR_TYPE_LIST_NM: [list of chars types...]}
+    """
+    resp_json = TEST_CLIENT.get(ep.CLOSETBROWSE_LIST_W_NS).get_json()
+    assert len(resp_json[ep.CLOSETBROWSE_LIST_NM]) > 0
+
+
+def test_get_clothing_details():
+    """
+    """
+    resp_json = TEST_CLIENT.get(f'{ep.CLOSETBROWSE_DETAILS}/{TEST_CLOTHING_TYPE}').get_json()
+    assert TEST_CLOTHING_TYPE in resp_json
+    assert isinstance(resp_json[TEST_CLOTHING_TYPE], dict)
 
 
 def test_login():
