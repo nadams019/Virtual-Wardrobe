e4e61bd66eceb23128e6629f587aa9849fe75d2e
bethanysaunders <bsaun99@gmail.com>
2022-12-14 00:17:46 -0500

    adding aesthetics types to test_endpoints.py

--- server/tests/test_endpoints.py
+++ server/tests/test_endpoints.py
@@ -2,10 +2,12 @@
 import server.endpoints as ep
 import db.users as usr
 import db.closet_browse as brwse
+import db.aesthetics_types as aes
 
 
 TEST_CLIENT = ep.app.test_client()
 TEST_CLOTHING_TYPE = 'Clothing'
+TEST_AES_TYPE = 'Grunge'
 
 
 def test_hello():
@@ -23,15 +25,14 @@ SAMPLE_USER = {
     usr.FULL_NAME: 'Sample User',
 }
 
-"""
+
 def test_add_user():
-
-    # Test adding a user.
-
+    """
+    Test adding a user.
+    """
     resp = TEST_CLIENT.post(ep.USER_ADD, json=SAMPLE_USER)
-    assert resp.get_json()
+    assert usr.user_exists(SAMPLE_USER_NM)
     usr.del_user(SAMPLE_USER_NM)
-"""
 
 
 def test_get_user_list():
@@ -43,6 +44,34 @@ def test_get_user_list():
     resp = TEST_CLIENT.get(ep.USER_LIST_W_NS)
     resp_json = resp.get_json()
     assert isinstance(resp_json[ep.USER_LIST_NM], list)
+
+
+def test_get_aesthetic_type_list():
+    """
+    See if we can get an aesthetic type list properly.
+    Return should look like:
+        {CHAR_TYPE_LIST_NM: [list of chars types...]}
+    """
+    resp_json = TEST_CLIENT.get(ep.AES_TYPE_LIST_W_NS).get_json()
+    assert isinstance(resp_json[ep.AES_TYPE_LIST_NM], list)
+
+
+def test_get_aesthetic_type_list_not_empty():
+    """
+    See if we can get an aesthetic type list properly.
+    Return should look like:
+        {AES_TYPE_LIST_NM: [list of chars types...]}
+    """
+    resp_json = TEST_CLIENT.get(ep.AES_TYPE_LIST_W_NS).get_json()
+    assert len(resp_json[ep.AES_TYPE_LIST_NM]) > 0
+
+
+def test_get_aesthetic_type_details():
+    """
+    """
+    resp_json = TEST_CLIENT.get(f'{ep.AES_TYPE_DETAILS_W_NS}/{TEST_AES_TYPE}').get_json()
+    assert TEST_AES_TYPE in resp_json
+    assert isinstance(resp_json[TEST_AES_TYPE], dict)
 
 
 SAMPLE_ITEM_NM = 'SampleItem'
