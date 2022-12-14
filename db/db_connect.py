e0af665afc07d9413d7054b2ff98cee85a2c8aa1
bethanysaunders <bsaun99@gmail.com>
2022-12-13 19:51:07 -0500

    adding remote mongo connection and deleting games

--- db/db_connect.py
+++ db/db_connect.py
@@ -22,13 +22,13 @@ def connect_db():
     if client is None:  # not connected yet!
         print("Setting client because it is None.")
         if os.environ.get("CLOUD_MONGO", LOCAL) == CLOUD:
-            password = os.environ.get("GAME_MONGO_PW")
+            password = os.environ.get("CLOSET_MONGO_PW")
             if not password:
                 raise ValueError('You must set your password '
                                  + 'to use Mongo in the cloud.')
             print("Connecting to Mongo in the cloud.")
-            client = pm.MongoClient(f'mongodb+srv://gcallah:{password}'
-                                    + '@cluster0.eqxbbqd.mongodb.net/'
+            client = pm.MongoClient(f'mongodb+srv://bsaun99:{password}'
+                                    + '@cluster0.tdferfh.mongodb.net/'
                                     + '?retryWrites=true&w=majority')
             # PA recommends these settings:
             # + 'connectTimeoutMS=30000&'
---
+++ wardrobe_mongo.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# Don't check this file into any repo!!!
+
+export CLOUD_MONGO=1
+export CLOSET_MONGO_PW='nyutandonSwe2022!'
