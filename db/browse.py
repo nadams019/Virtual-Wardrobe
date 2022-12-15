0500ecb188a503fbdb8b2f368f41e13ddd9706cd
bethanysaunders <bsaun99@gmail.com>
2022-12-15 02:31:27 -0500

    changing browse.py

--- db/browse.py
+++ db/browse.py
@@ -33,29 +33,33 @@ Random: True or False
 
 
 def get_clothing_details(clothing):
-    dbc.connect_db()
-    return dbc.fetch_one(CLOTHING_COLLECT, {CLOTHING_KEY: clothing})
+    return closet.get(clothing, None)
+    # dbc.connect_db()
+    # return dbc.fetch_one(CLOTHING_COLLECT, {CLOTHING_KEY: clothing})
 
 
 def clothing_exists(name):
     """
     Returns a clothing item exists.
     """
-    return get_clothing_details(name) is not None
+    return name in closet
+    # return get_clothing_details(name) is not None
 
 
 def get_clothing_dict():
-    dbc.connect_db()
-    return dbc.fetch_all_as_dict(CLOTHING_KEY, CLOTHING_COLLECT)
+    return closet
+    # dbc.connect_db()
+    # return dbc.fetch_all_as_dict(CLOTHING_KEY, CLOTHING_COLLECT)
 
 
 def get_clothes():
-    dbc.connect_db()
-    return dbc.fetch_all(CLOTHING_COLLECT)
+    # dbc.connect_db()
+    # return dbc.fetch_all(CLOTHING_COLLECT)
+    return list(closet.keys())
 
 
 def add_clothing(name, details):
-    doc = details
+    # doc = details
     if not isinstance(name, str):
         raise TypeError(f'Wrong type for name: {type(name)=}')
     if not isinstance(details, dict):
@@ -63,14 +67,15 @@ def add_clothing(name, details):
     for field in REQUIRED_FLDS:
         if field not in details:
             raise ValueError(f'Required {field=} missing from details.')
-    dbc.connect_db()
-    doc[CLOTHING_KEY] = name
-    return dbc.insert_one(CLOTHING_COLLECT, doc)
+    closet[name] = details
+    # dbc.connect_db()
+    # doc[CLOTHING_KEY] = name
+    # return dbc.insert_one(CLOTHING_COLLECT, doc)
 
 
 def del_clothing(name):
-    # del closet[name]
-    return dbc.del_one(CLOTHING_COLLECT, {CLOTHING_KEY: name})
+    del closet[name]
+    # return dbc.del_one(CLOTHING_COLLECT, {CLOTHING_KEY: name})
 
 
 def main():
