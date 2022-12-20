1856c9839b610f789a4b523d2db96e4a4401cb47
Aadiba Haque <41478933+adibsxion19@users.noreply.github.com>
2022-12-20 05:31:06 -0500

    Update test_endpoints.py

--- server/tests/test_endpoints.py
+++ server/tests/test_endpoints.py
@@ -101,7 +101,7 @@ def test_get_clothing_list():
     Return should look like:
         {CLOSET_LIST_NM: [list of users types...]}
     """
-    resp = TEST_CLIENT.get(ep.CLOSET_LIST_W_NS).get_json()
+    resp = TEST_CLIENT.get(ep.CLOSET_LIST_W_NS)
     resp_json = resp.get_json()
     assert isinstance(resp_json[ep.CLOSET_LIST_NM], list)
 
