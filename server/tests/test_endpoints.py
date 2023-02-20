c032b984c51c1a1b9682edf1ba378bb880dcc3a9
Nakeya Adams <naa498@nyu.edu>
2023-02-19 19:02:05 -0500

    Checking to see if another test case works

--- server/tests/test_endpoints.py
+++ server/tests/test_endpoints.py
@@ -99,8 +99,8 @@ def test_get_clothing_list():
     Return should look like:
         {CLOSETBROWSE_LIST_NM: [list of users types...]}
     """
-    resp_json = TEST_CLIENT.get(ep.CLOSET_LIST_W_NS).get_json()
-    assert isinstance(resp_json[ep.CLOSET_LIST_NS], list)
+    resp_json = TEST_CLIENT.get(ep.CLOSET_LIST_NM).get_json()
+    assert isinstance(resp_json[ep.CLOSET_LIST_W_NS], list)
 
 '''
 def test_add_clothing_post():
