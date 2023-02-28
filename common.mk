45ff5a5c1f0bdfde210851a36b88b8725b0e50a1
Aadiba Haque <41478933+adibsxion19@users.noreply.github.com>
2023-02-27 21:45:29 -0500

    add line --cov-branch

--- common.mk
+++ common.mk
@@ -1,5 +1,5 @@
 LINTER = flake8
-PYTESTFLAGS = -vv --verbose --tbshort --cov=$(PKG) --cov-report term-missing
+PYTESTFLAGS = -vv --verbose --tbshort --cov=$(PKG) --cov-branch --cov-report term-missing
 
 FORCE:
 
