e9571e3acb4d3df1184ed34021ced8b0fe89ebaf
Aadiba Haque <41478933+adibsxion19@users.noreply.github.com>
2023-04-16 15:47:42 -0400

    testing get_contact_list

--- server/tests/test_endpoints.py
+++ server/tests/test_endpoints.py
@@ -121,7 +121,7 @@ def test_login():
     response = TEST_CLIENT.get(f'{ep.LOGIN_NS}').get_json()
     assert response.status == "Successfully logged in"
 '''
-'''
+
 SAMPLE_CONTACT_NM = 'SampleContact'
 SAMPLE_ITEM = {
     cnts: SAMPLE_CONTACT_NM,
@@ -135,4 +135,28 @@ def test_get_contact_list():
     resp = TEST_CLIENT.get(ep.CONTACTS_LIST_W_NS)
     resp_json = resp.get_json()
     assert isinstance(resp_json[ep.CONTACTS_LIST_NM], list)
-'''
+
+#def test_add_contact():
+#    """
+#    Test adding a contact request.
+#    """
+#    resp = TEST_CLIENT.post(ep.CONTACTS_ADD, json=SAMPLE_USER)
+#    assert cnts.user_exists(SAMPLE_USER_NM)
+#    usr.del_user(SAMPLE_USER_NM)
+# def test_get_contact_list():
+#     """
+#     See if we can get a contact list properly.
+#     Return should look like:
+#         {CONTACTS_LIST_NM: [list of users types...]}
+#     """
+#     resp_json = TEST_CLIENT.get(ep.CONTACTS_LIST_NM).get_json()
+#     assert isinstance(resp_json[ep.CONTACTS_LIST_W_NS], list)
+
+# def test_get_contact_details():
+#     """
+#     See if we can get contact details
+#     """
+#     resp_json = TEST_CLIENT.get(f'{ep.CONTACTS_DETAILS_W_NS}/{TEST_CONTACTS_TYPE}').get_json()
+#     assert TEST_CONTACTS_TYPE in resp_json
+#     assert isinstance(resp_json[TEST_CONTACTS_TYPE], dict)
+
