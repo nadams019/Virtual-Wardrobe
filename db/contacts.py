9be189d5f452fb6a638844801d0ebc5a1e46fd79
Aadiba Haque <41478933+adibsxion19@users.noreply.github.com>
2023-04-16 15:50:55 -0400

    changing variable inputted for contact_exists

--- db/contacts.py
+++ db/contacts.py
@@ -30,8 +30,8 @@ def add_contact(name, details):
             raise ValueError(f'Required {field=} missing from details.')
     contacts[name] = details
 
-def contact_exists(email):
-  return email
+def contact_exists(name):
+  return name
 
 def get_contacts():
     return list(contacts.keys())
