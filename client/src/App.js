11a05bb54d816ce76414f9b0a4e72375987db951
Nakeya Adams <naa498@nyu.edu>
2023-04-26 00:25:30 -0400

    Connecting front end to backend using react

---
+++ client/src/App.js
@@ -0,0 +1,20 @@
+import React, { useState, useEffect } from "react";
+import "./App.css";
+
+function App() {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:8000/message")
+      .then((res) => res.json())
+      .then((data) => setMessage(data.message));
+  }, []);
+
+  return (
+    <div className="App">
+      <h1>{message}</h1>
+    </div>
+  );
+}
+
+export default App
