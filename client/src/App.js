ccb1149f9f1eabb612fcce59e8b246eac845c436
bismahshahid <bismahshahid786@gmail.com>
2023-04-27 10:44:58 -0400

    adding browse button to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,46 @@
 // App.js file to connect the front end to the backend file
 
-import React, { useState, useEffect } from "react";
-import "./App.css";
+import { useState } from 'react';
+import axios from 'axios';
+import logo from './logo.svg';
+import './App.css';
+function StartButton({ setText }) {
+  const onClick = () => { axios.get("http://127.0.0.1:8000/browse/dict")
+.then((response) => {
+const items = response.data.Data[0];
+const stringItems = JSON.stringify(items);
+setText(stringItems);
+} )
+.catch();
+};
+  return ( <button onClick={onClick}>Browse!</button>);
+}
+
+function aestheticButton({ setText }) {
+  const onClick = () => { axios.get("http://127.0.0.1:8000/browse/dict")
+.then((response) => {
+const items = response.data.Data[0];
+const stringItems = JSON.stringify(items);
+setText(stringItems);
+} )
+.catch();
+};
+  return ( <button onClick={onClick}>Click for aesthetic quiz!</button>);
+}
+
 
 function App() {
-  const [message, setMessage] = useState("");
-
-  useEffect(() => {
-    fetch("http://localhost:8000/message")
-      .then((res) => res.json())
-      .then((data) => setMessage(data.message));
-  }, []);
-
+const [text, setText] = useState('');
   return (
     <div className="App">
-      <h1>{message}</h1>
+      <header className="App-header">
+      <StartButton setText={setText} />
+          <aestheticButton setText={setText} />
+      {text}
+      </header>
     </div>
   );
 }
 
-export default App
+export default App;
+
