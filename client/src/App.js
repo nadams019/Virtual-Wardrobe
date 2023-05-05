1f20ac4df1a4f892550e585779e6a3bd025fb6db
Aadiba Haque <aadibahaque0219@gmail.com>
2023-05-05 10:33:57 -0400

    working on fixing app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,48 +1,109 @@
 // App.js file to connect the front end to the backend file
-
-import { useState } from 'react';
+//
+import { useState, useEffect } from 'react';
 import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
+import { Link } from 'react-router-dom';
 
-
-function StartButton({ setText }) {
-  const onClick = () => { axios.get("https://virtual-wardrobe.herokuapp.com/browse/dict")
-.then((response) => {
-const items = response.data.Data[0];
-const stringItems = JSON.stringify(items);
-setText(stringItems);
-} )
-.catch();
-};
-  return ( <button onClick={onClick}>Browse!</button>);
-}
-
-//function AestheticButton({ setText }) {
-//  const onClick = () => { axios.get("http://127.0.0.1:8000/aesthetics_types/dict")
-//.then((response) => {
-//const items = response.data.Data[0];
+const API_BASE_URL = 'https://virtual-wardrobe.herokuapp.com';
+const API_BROWSE_DICT_URL = `${API_BASE_URL}/browse/dict`;
+// function StartButton({ setText }) {
+//   const onClick = () => { axios.get("https://virtual-wardrobe.herokuapp.com/browse/dict")
+// .then((response) => {
+// const items = response.data.Data[0];
 // const stringItems = JSON.stringify(items);
 // setText(stringItems);
 // } )
 // .catch();
 // };
-//   return ( <button onClick={onClick}>Click for aesthetic quiz!</button>);
+//   return ( <button onClick={onClick}>Browse!</button>);
+// }
+async function fetchItems() {
+  try {
+    const onClick = () => { axios.get(API_BROWSE_DICT_URL).then((response) => {
+      const items = response.data.Data[0];
+      return items;
+  } ).catch (error) {
+    console.error(error);
+    return null;
+  }
+}
+
+// function BrowsePage({ items }) {
+//   return (
+//     <div>
+//       {items.map((item) => (
+//         <div key={item.id}>
+//           <h2>{item.name}</h2>
+//           <p>{item.description}</p>
+//           <img src={item.image_url} alt={item.name} />
+//         </div>
+//       ))}
+//     </div>
+//   );
 //}
 
-
 function App() {
-const [text, setText] = useState('');
+  const [items, setItems] = useState([]);
+
+  async function loadItems() {
+    const items = await fetchItems();
+    if (items) {
+      setItems(items);
+    }
+  }
+
+  useEffect(() => {
+    loadItems();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-      <StartButton setText={setText} />
-      {text}
-      </header>
+      <div className="App">
+        <header className="App-header">
+          <Link to="/browse">
+            <button>Browse Items</button>
+          </Link>
+        </header>
     </div>
   );
 }
 
 export default App;
+//<BrowsePage items={items} />
+// import { useState } from 'react';
+// import axios from 'axios';
+// import logo from './logo.svg';
+// import './App.css';
+//
+//
 
-//<AestheticButton setText={setText} /
+//
+// //function AestheticButton({ setText }) {
+// //  const onClick = () => { axios.get("http://127.0.0.1:8000/aesthetics_types/dict")
+// //.then((response) => {
+// //const items = response.data.Data[0];
+// // const stringItems = JSON.stringify(items);
+// // setText(stringItems);
+// // } )
+// // .catch();
+// // };
+// //   return ( <button onClick={onClick}>Click for aesthetic quiz!</button>);
+// //}
+//
+//
+// function App() {
+// const [text, setText] = useState('');
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//       <StartButton setText={setText} />
+//       {text}
+//       </header>
+//     </div>
+//   );
+// }
+//
+// export default App;
+//
+// //<AestheticButton setText={setText} /
